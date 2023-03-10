a67481044003392cf33225669e1f835a865b87cf
gcostacoelho <gcostacoelho2003@gmail.com>
2023-03-10 12:05:58 -0300

    adding-openapi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('Blood Bank')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
+  
+  
+  @Get('teste1')
   getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('teste2')
+  getHello2(): string {
     return this.appService.getHello();
   }
 }
