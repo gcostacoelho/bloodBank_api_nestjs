6b6a9f2fa25c281c2127aa00781697b69da140a1
gcostacoelho <gcostacoelho2003@gmail.com>
2023-03-10 12:20:58 -0300

    folders-organization

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { AppService } from './app.service';
+import { AppService } from '../services/app.service';
 
 @ApiTags('Blood Bank')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
   
   @Get('teste1')
   getHello(): string {
